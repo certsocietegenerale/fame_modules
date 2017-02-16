ca87eeb77dcc3755b9cc3e40fe123617bfcce0a9
Gael Muller <muller.gael@gmail.com>
2017-02-16 13:00:00 +0100

    handle APK files correctly

--- processing/zip.py
+++ processing/zip.py
@@ -14,21 +14,30 @@ class Zip(ProcessingModule):
         tmpdir = tempdir()
 
         zf = ZipFile(target)
-        for name in zf.namelist():
-            try:
-                filepath = zf.extract(name, tmpdir)
-                if os.path.isfile(filepath):
-                    self.add_extracted_file(filepath)
-            except RuntimeError:
-                for password in ['virus', 'infected']:
-                    try:
-                        filepath = zf.extract(name, tmpdir, pwd=password)
-                        if os.path.isfile(filepath):
-                            self.add_extracted_file(filepath)
-                        break
-                    except RuntimeError:
-                        pass
-                else:
-                    self.log('error', 'Could not extract {}'.format(name))
+
+        namelist = zf.namelist()
+
+        if 'classes.dex' in namelist and 'META-INF/MANIFEST.MF' in namelist:
+            self.change_type(target, 'apk')
+            self.results = {
+                'message': 'File type was changed to apk.'
+            }
+        else:
+            for name in namelist:
+                try:
+                    filepath = zf.extract(name, tmpdir)
+                    if os.path.isfile(filepath):
+                        self.add_extracted_file(filepath)
+                except RuntimeError:
+                    for password in ['virus', 'infected']:
+                        try:
+                            filepath = zf.extract(name, tmpdir, pwd=password)
+                            if os.path.isfile(filepath):
+                                self.add_extracted_file(filepath)
+                            break
+                        except RuntimeError:
+                            pass
+                    else:
+                        self.log('error', 'Could not extract {}'.format(name))
 
         return True
