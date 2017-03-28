be1645d1b703f1a039b028163ba00145b7b263a1
Gael Muller <muller.gael@gmail.com>
2017-03-28 22:28:00 +0200

    office_macros: fix cases where analysis is None

--- processing/office_macros/office_macros.py
+++ processing/office_macros/office_macros.py
@@ -42,7 +42,6 @@ class OfficeMacros(ProcessingModule):
 
         # code is inspired by 'reveal' method in olevba
         analysis = vba.analyze_macros(show_decoded_strings=True)
-        analysis = sorted(analysis, key=lambda type_decoded_encoded: len(type_decoded_encoded[2]), reverse=True)
 
         # extract all macros code
         for (_, _, _, vba_code) in vba.extract_all_macros():
@@ -53,14 +52,16 @@ class OfficeMacros(ProcessingModule):
             self.results['analysis']['Form String'].append(form_string.decode('utf-8', errors='replace'))
 
         # extract all analysis
-        for kw_type, keyword, description in analysis:
-            # and replace obfuscated strings
-            if kw_type in ['VBA string', 'Dridex string', 'Base64 String', 'Hex String']:
-                if olevba.is_printable(keyword):
-                    keyword = keyword.replace('"', '""')
-                    self.results['macros'] = self.results['macros'].replace(description, '"%s"' % keyword)
-                    self.results['analysis'][kw_type].append((keyword.decode('utf-8', errors='replace'), description.decode('utf-8', errors='replace')))
-            else:
-                self.results['analysis'][kw_type].append((keyword, description))
+        if analysis:
+            analysis = sorted(analysis, key=lambda type_decoded_encoded: len(type_decoded_encoded[2]), reverse=True)
+            for kw_type, keyword, description in analysis:
+                # and replace obfuscated strings
+                if kw_type in ['VBA string', 'Dridex string', 'Base64 String', 'Hex String']:
+                    if olevba.is_printable(keyword):
+                        keyword = keyword.replace('"', '""')
+                        self.results['macros'] = self.results['macros'].replace(description, '"%s"' % keyword)
+                        self.results['analysis'][kw_type].append((keyword.decode('utf-8', errors='replace'), description.decode('utf-8', errors='replace')))
+                else:
+                    self.results['analysis'][kw_type].append((keyword, description))
 
         return len(self.results['macros']) > 0
