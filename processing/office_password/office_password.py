f74e01e8bd1a4fa0284b5b2959d75c20946bdeff
Heat Miser <heatmiser.contact@gmail.com>
2019-06-13 18:36:44 +0200

    Added: plugin to decrypt password protected office documents based on msoffcrypto-tool

---
+++ processing/office_password/office_password.py
@@ -0,0 +1,45 @@
+import os
+from msoffcrypto import OfficeFile
+
+from fame.core.module import ProcessingModule
+from fame.common.utils import tempdir
+
+
+class OfficePassword(ProcessingModule):
+    name = "office_password"
+    description = "Decrypt password protected office documents."
+    acts_on = "word,excel,powerpoint"
+
+    config = [
+        {
+            "name": "password_candidates",
+            "type": "text",
+            "default": "1234\n123456\ninfected",
+            "description": "List of passwords to try when decrypting an encrypted Office document (one per line)."
+        }
+    ]
+
+    def each(self, target):
+        tmpdir = tempdir()
+        password_candidates = self.password_candidates.split("\n")
+
+        with open(target, "rb") as myfile:
+
+            document = OfficeFile(myfile)
+
+            for password in password_candidates:
+                password = password.strip()
+                try:
+                    document.load_key(password=password)
+                    out_file = tmpdir + os.path.sep + "decrypted_" + os.path.basename(target)
+                    with open(out_file, "wb") as output:
+                        document.decrypt(output)
+                    if os.path.isfile(out_file):
+                        self.add_extracted_file(out_file)
+                    break
+                except:
+                    pass
+            else:
+                self.log('error', 'Could not extract {} (password not known)'.format(target))
+
+        return True
