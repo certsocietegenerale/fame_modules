6583f66688c60512c57b80558eedfee4aa0d7841
@ <@>
2020-04-22 14:43:25 -0500

    dnsdumpster intel from hackertarget

---
+++ processing/dnsdumpster/dnsdumpster.py
@@ -0,0 +1,102 @@
+import os
+import sys
+import json
+
+"""
+Todo:
+    * Add json output as support_file
+    * Snag that fancy domain graph off dnsdumpster?
+    * Snag that fancy dns excel file off dnsdumpster?
+
+"""
+
+from fame.core.module import ProcessingModule
+from fame.common.constants import VENDOR_ROOT
+from fame.common.exceptions import ModuleInitializationError
+
+try:
+    sys.path.append(os.path.join(VENDOR_ROOT, 'dnsdmpstr'))
+    import dnsdmpstr
+    has_dnsdump = True
+except ImportError:
+    has_dnsdump = False
+
+try:
+    import tldextract
+    has_tldextract = True
+except ImportError:
+    has_tldextract = False
+
+class DnsDumpster(ProcessingModule):
+    name = "dnsdumpster"
+    description = "Enumerate domain DNS data."
+    acts_on = ['url']
+
+    config = [
+        {
+            'name': 'reverse_dns',
+            'type': 'bool',
+            'default': 'false',
+            'description': 'List reverse DNS entries from DnsDumpster.'
+        },
+        {
+            'name': 'page_links',
+            'type': 'bool',
+            'default': 'false',
+            'description': 'Grab page links using HackerTarget.'
+        },
+        {
+            'name': 'http_headers',
+            'type': 'bool',
+            'default': 'false',
+            'description': 'Grab HTTP server headers from URL using HackerTarget.'
+        },
+    ]
+
+    def initialize(self):
+        if not has_dnsdump:
+            raise ModuleInitializationError(self, "Missing dependancy: dnsdmpstr")
+        if not has_tldextract:
+            raise ModuleInitializationError(self, "Missing dependancy: tldextract")
+
+    def each(self, target):
+        self.results = {}
+
+        # Get the root domain
+        url = target
+        domain = tldextract.extract(url)
+        root_domain = domain.domain + '.' + domain.suffix
+
+        # Initialize dnsdmpstr and enumerate the data
+        dnsdump = dnsdmpstr.dnsdmpstr()
+
+        # DNS Data
+        self.log("debug", 'gathering dns data...')
+        self.results['dns_data'] = dnsdump.dnslookup(root_domain)
+
+        # Reverse DNS
+        if self.reverse_dns:
+            self.log("debug", 'gathering reverse dns data...')
+            self.results['reverse_dns'] = dnsdump.reversedns(root_domain)
+
+        # HTTP Headers
+        if self.http_headers:
+            self.log("debug", 'gathering url headers...')
+            # Query HackerTarget with a cleaned up target URL
+            dest = url.split('?')[0]  # reduce dnsdmpstr errors
+            dest = dest.split('#')[0] # 
+            headers = dnsdump.httpheaders(dest)
+            if not "error" in headers:
+                self.results['headers'] = headers
+
+        # Page Links
+        if self.page_links:
+            self.log("debug", 'gathering url links...')
+            # Query HackerTarget with a cleaned up target URL
+            dest = url.split('?')[0]  # reduce dnsdmpstr errors
+            dest = dest.split('#')[0] #
+            links = dnsdump.pagelinks(dest)
+            if not "url is invalid" in links:
+                self.results['links'] = links
+
+        return True
---
+++ processing/dnsdumpster/install.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from git import Repo
+
+sys.path.append(os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "..", "..")))
+
+from fame.common.constants import VENDOR_ROOT
+
+
+def main():
+    dnsdmpstr_path = os.path.join(VENDOR_ROOT, 'dnsdmpstr')
+
+    if os.path.exists(dnsdmpstr_path):
+        repo = Repo(dnsdmpstr_path)
+        repo.remotes.origin.pull()
+    else:
+        Repo.clone_from("https://github.com/zeropwn/dnsdmpstr.git", dnsdmpstr_path)
+
+if __name__ == '__main__':
+    main()
