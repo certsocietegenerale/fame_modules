67d9393e807035a3ca0b8c67a1adcf9dee524fb6
@ <@>
2020-04-23 11:34:01 -0500

    fixed vendor import depth

--- processing/dnsdumpster/install.py
+++ processing/dnsdumpster/install.py
@@ -2,7 +2,7 @@ import os
 import sys
 from git import Repo
 
-sys.path.append(os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "..", "..")))
+sys.path.append(os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "..", "..", "..")))
 
 from fame.common.constants import VENDOR_ROOT
 
