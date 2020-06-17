d6579dbd64af0a667de77b98b13dd8bea6999751
Heat Miser <github@heat-miser.net>
2020-06-17 11:21:57 +0200

    Added plugin using xlmdeobfuscator from DissectMalware (#37)
    * Added: xlmdeofuscator plugin
    
    * dockerized xlmeobfuscator
    
    * modified docker file for xmld plugin
    
    * updated dockerfile for xlmd plugin
    
    * removed check for xlmd dependency because dockerized
    
    * bugfix
    
    * bugfix
    
    * bugfix in docker management in xlmd
    
    * bugfix in xlmd
    
    * bugfix in xlmd
    
    * bugfix in xlmd
    
    * bugfix in xlmd
    
    * improvement in xlmd
    
    * modifications in xlmd
    
    * modifications in xlmd
    
    * bugfix in xlmd

---
+++ processing/xlmdeobfuscator/xlm_deobfuscator.py
@@ -0,0 +1,59 @@
+import re
+import os
+import json
+from fame.common.utils import tempdir
+from shutil import copyfile
+from fame.core.module import ProcessingModule
+from fame.common.exceptions import ModuleInitializationError
+
+from ..docker_utils import HAVE_DOCKER, docker_client, temp_volume
+
+
+def str_reverse(match):
+    return match.group(1)[::-1]
+
+
+class XLMDeobfuscator(ProcessingModule):
+    name = "xlm_deobfuscator"
+    description = "Extract and analyze Excel 4.0 macros."
+    acts_on = ["excel", "xls", "xlsm"]
+
+    def initialize(self):
+        if not HAVE_DOCKER:
+            raise ModuleInitializationError(self, "Missing dependency: docker")
+
+        return True
+
+    def run_xlmd(self, target):
+
+        args = "-n --file /data/{} --export-json /data/output/results.json".format(target)
+
+        # start the right docker
+        return docker_client.containers.run(
+            'fame/xlmdeobfuscator',
+            args,
+            volumes={self.outdir: {'bind': '/data', 'mode': 'rw'}},
+            stderr=True,
+            remove=True
+        )
+
+    def each(self, target):
+        self.results = {
+            'macros': u''
+        }
+
+        self.outdir = temp_volume(target)
+        results_dir = os.path.join(self.outdir, "output")
+
+        self.run_xlmd(os.path.basename(target))
+
+        regex_url = r"\w+:(\/\/)[^\s\"]+"
+        reg = re.compile(regex_url)
+        with open(os.path.join(results_dir, "results.json")) as results_json:
+            data = json.load(results_json)
+            for record in data['records']:
+                self.results["macros"] = self.results["macros"] + "\n" + record['formula']
+                for match in reg.finditer(record['formula']):
+                    self.add_ioc(match.group(0))
+
+        return len(self.results["macros"]) > 0
