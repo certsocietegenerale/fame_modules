7d4bf2685442ef2d0299e9af4b922688c7ad1422
Gael Muller <muller.gael@gmail.com>
2020-12-13 18:23:28 +0100

    feat: port virustotal_download, exiftool, document_preview, url_preview, pdf, zip & extract

--- processing/url_preview/url_preview.py
+++ processing/url_preview/url_preview.py
@@ -9,8 +9,8 @@ from fame.common.utils import tempdir
 
 from ..docker_utils import HAVE_DOCKER, docker_client
 
-class MyHTMLParser(HTMLParser):
 
+class MyHTMLParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
         self._URLS = []
@@ -18,11 +18,15 @@ class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         if tag == "a":
             for attr in attrs:
-                if attr[0].lower() == "href" and (attr[1].lower().startswith("http") or attr[1].lower().startswith("ftp")):
+                if attr[0].lower() == "href" and (
+                    attr[1].lower().startswith("http") or attr[1].lower().startswith("ftp")
+                ):
                     self._URLS.append(attr[1])
         if tag == "form":
             for attr in attrs:
-                if attr[0].lower() == "action" and (attr[1].lower().startswith("http") or attr[1].lower().startswith("ftp")):
+                if attr[0].lower() == "action" and (
+                    attr[1].lower().startswith("http") or attr[1].lower().startswith("ftp")
+                ):
                     self._URLS.append(attr[1])
         if tag == "meta":
             for attr in attrs:
@@ -30,24 +34,28 @@ class MyHTMLParser(HTMLParser):
                     for att in attrs:
                         if att[0].lower() == "content":
                             url = att[1].split("=")
-                            if len(url) > 1 and (url[1].lower().startswith("http") or url[1].lower().startswith("ftp")):
+                            if len(url) > 1 and (
+                                url[1].lower().startswith("http")
+                                or url[1].lower().startswith("ftp")
+                            ):
                                 self._URLS.append(url[1])
 
     def get_urls(self):
         return self._URLS
 
+
 class UrlPreview(ProcessingModule):
 
-    name = 'url_preview'
-    description = 'List all redirections from an URL request and take a screenshot'
-    acts_on = ['url', 'html']
+    name = "url_preview"
+    description = "List all redirections from an URL request and take a screenshot"
+    acts_on = ["url", "html"]
 
     config = [
         {
-            'name': 'network_idle_timeout',
-            'type': 'integer',
-            'default': 500,
-            'description': 'Specify the network idle timeout (ms)',
+            "name": "network_idle_timeout",
+            "type": "integer",
+            "default": 500,
+            "description": "Specify the network idle timeout (ms)",
         }
     ]
 
@@ -58,44 +66,45 @@ class UrlPreview(ProcessingModule):
         return True
 
     def save_preview(self, outdir):
-        filepath = os.path.join(outdir, 'output.png')
+        filepath = os.path.join(outdir, "output.png")
 
         if os.path.isfile(filepath):
-            self.add_support_file('preview', filepath)
+            self.add_support_file("preview", filepath)
             return True
         else:
             return False
 
     def preview(self, url):
-        args = 'node {} {} {}'.format(
-            '/script.js', url, self.network_idle_timeout)
+        args = "node {} {} {}".format("/script.js", url, self.network_idle_timeout)
 
         # start the right docker
-        return docker_client.containers.run(
-            'fame/url_preview',
+        output = docker_client.containers.run(
+            "fame/url_preview",
             args,
-            volumes={self.outdir: {'bind': '/data', 'mode': 'rw'}},
+            volumes={self.outdir: {"bind": "/data", "mode": "rw"}},
             stderr=True,
-            remove=True
+            remove=True,
         )
+
+        if type(output) is bytes:
+            output = output.decode("utf-8", errors="replace")
+
+        return output
 
     def save_output(self, output):
         for line in output.splitlines():
-            if line.startswith('redirect'):
+            if line.startswith("redirect"):
                 redirect = line.split()
-                self.results['redirections'].append(redirect[1])
+                self.results["redirections"].append(redirect[1])
             else:
                 self.log("debug", line)
 
         # add last element as target url
         # then delete last element
-        self.results['target'] = self.results['redirections'].pop()
+        self.results["target"] = self.results["redirections"].pop()
 
     def each_with_type(self, target, filetype):
-        self.results = {
-            'redirections': [],
-            'target': None
-        }
+        self.results = {"redirections": [], "target": None}
 
         # Create temporary directory to get results
         self.outdir = tempdir()
@@ -108,14 +117,14 @@ class UrlPreview(ProcessingModule):
 
         # add http protocol if missing
         # requests lib needs it
-        if filetype == "url" and not target.startswith('http'):
-            target = 'http://{}'.format(target)
-            
+        if filetype == "url" and not target.startswith("http"):
+            target = "http://{}".format(target)
+
         if filetype == "url":
             self.add_ioc(target)
 
         # output dir
-        results_dir = os.path.join(self.outdir, 'output')
+        results_dir = os.path.join(self.outdir, "output")
 
         if not os.path.isdir(results_dir):
             os.mkdir(results_dir)
@@ -135,11 +144,11 @@ class UrlPreview(ProcessingModule):
             for url in parser.get_urls():
                 self.add_ioc(url)
 
-        if len(self.results['redirections']) > 0:
+        if len(self.results["redirections"]) > 0:
             # save redirections as observable
-            self.add_ioc(self.results['redirections'], ['redirection'])
+            self.add_ioc(self.results["redirections"], ["redirection"])
 
             # save target as observable
-            self.add_ioc(self.results['target'])
+            self.add_ioc(self.results["target"])
 
-        return len(self.results['redirections']) > 0 or screenshot
+        return len(self.results["redirections"]) > 0 or screenshot
--- processing/zip/zip.py
+++ processing/zip/zip.py
@@ -15,26 +15,24 @@ class LegacyZip(ProcessingModule):
             "name": "password_candidates",
             "type": "text",
             "default": "virus\ninfected",
-            "description": "List of passwords to try when unpacking an encrypted ZIP file (one per line)."
+            "description": "List of passwords to try when unpacking an encrypted ZIP file (one per line).",
         },
         {
             "name": "maximum_extracted_files",
             "type": "integer",
             "default": 5,
-            "description": "If there are more files than this value in the archive, files will not be extracted."
+            "description": "If there are more files than this value in the archive, files will not be extracted.",
         },
         {
             "name": "maximum_automatic_analyses",
             "type": "integer",
             "default": 1,
-            "description": "If there are more files than this value in the archive, no analyses will be automatically created for extracted files."
-        }
+            "description": "If there are more files than this value in the archive, no analyses will be automatically created for extracted files.",
+        },
     ]
 
     def each(self, target):
-        self.results = {
-            'warnings': []
-        }
+        self.results = {"warnings": []}
 
         tmpdir = tempdir()
 
@@ -44,9 +42,11 @@ class LegacyZip(ProcessingModule):
 
         namelist = zf.namelist()
 
-        if 'classes.dex' in namelist and 'META-INF/MANIFEST.MF' in namelist:
-            self.change_type(target, 'apk')
-            self.results['warnings'].append('File type was changed to apk, files were not extracted.')
+        if "classes.dex" in namelist and "META-INF/MANIFEST.MF" in namelist:
+            self.change_type(target, "apk")
+            self.results["warnings"].append(
+                "File type was changed to apk, files were not extracted."
+            )
         else:
             should_extract = len(namelist) <= self.maximum_extracted_files
             should_analyze = len(namelist) <= self.maximum_automatic_analyses
@@ -60,26 +60,36 @@ class LegacyZip(ProcessingModule):
                     except RuntimeError:
                         for password in password_candidates:
                             try:
-                                filepath = zf.extract(name, tmpdir, pwd=password.rstrip())
+                                filepath = zf.extract(
+                                    name, tmpdir, pwd=password.rstrip().encode("utf-8")
+                                )
                                 if os.path.isfile(filepath):
-                                    self.add_extracted_file(filepath, automatic_analysis=should_analyze)
+                                    self.add_extracted_file(
+                                        filepath, automatic_analysis=should_analyze
+                                    )
                                 break
                             except RuntimeError:
                                 pass
                         else:
-                            self.results['warnings'].append('Could not extract {} (password not known)'.format(name))
+                            self.results["warnings"].append(
+                                "Could not extract {} (password not known)".format(name)
+                            )
 
                 if not should_analyze:
-                    self.results['warnings'].append(
+                    self.results["warnings"].append(
                         "Archive contains more than {} files ({}), so no analysis was automatically created.".format(
-                            self.maximum_automatic_analyses, len(namelist)))
+                            self.maximum_automatic_analyses, len(namelist)
+                        )
+                    )
             else:
-                self.results['warnings'].append(
+                self.results["warnings"].append(
                     "Archive contains more than {} files ({}), so they were not extracted.".format(
-                        self.maximum_extracted_files, len(namelist)))
+                        self.maximum_extracted_files, len(namelist)
+                    )
+                )
 
-        if self.results['warnings']:
-            self.results['files'] = namelist
+        if self.results["warnings"]:
+            self.results["files"] = namelist
         else:
             self.results = None
 
