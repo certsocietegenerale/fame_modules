b00b05d602ff35b6eec828eec5bfb2b03e3ea095
Gael Muller <muller.gael@gmail.com>
2021-01-10 14:23:18 +0100

    feat: port reporting modules

--- reporting/mattermost.py
+++ reporting/mattermost.py
@@ -1,4 +1,3 @@
-
 import json
 
 from fame.common.exceptions import ModuleInitializationError
@@ -6,12 +5,14 @@ from fame.core.module import ReportingModule
 
 try:
     import requests
+
     HAVE_REQUESTS = True
 except ImportError:
     HAVE_REQUESTS = False
 
 try:
     from defang import defang
+
     HAVE_DEFANG = True
 except ImportError:
     HAVE_DEFANG = False
@@ -22,15 +23,11 @@ class Mattermost(ReportingModule):
     description = "Post message on Mattermost when an analysis is finished."
 
     config = [
+        {"name": "url", "type": "str", "description": "Incoming webhook URL."},
         {
-            'name': 'url',
-            'type': 'str',
-            'description': 'Incoming webhook URL.'
-        },
-        {
-            'name': 'fame_base_url',
-            'type': 'str',
-            'description': 'Base URL of your FAME instance, as you want it to appear in links.'
+            "name": "fame_base_url",
+            "type": "str",
+            "description": "Base URL of your FAME instance, as you want it to appear in links.",
         },
     ]
 
@@ -47,29 +44,36 @@ class Mattermost(ReportingModule):
             return False
 
     def done(self, analysis):
+        string = "Just finished analysis on {0}\n".format(
+            defang(", ".join(analysis._file["names"]))
+        )
 
-        string = "Just finished analysis on {0}\n".format(defang(', '.join(analysis._file['names'])))
+        if analysis["modules"]:
+            string += f"Selected Modules: {', '.join(analysis['modules'])}\n"
 
-        if analysis['modules'] is not None:
-            string += "modules used: "
-            for module in analysis['modules']:
-                string += "{0} ".format(module)
-            string += "\n"
+        if analysis["probable_names"]:
+            string += f"Probable Names: {', '.join(analysis['probable_names'])}\n"
 
-        if len(analysis['extractions']) > 0:
-            string += "Extractions: {0}\n".format(','.join([x['label'] for x in analysis['extractions']]))
+        if analysis["extractions"]:
+            string += f"Extractions: {', '.join([x['label'] for x in analysis['extractions']])}\n"
 
-        if len(analysis['iocs']) > 0:
-            string += "IOCs: {0}\n".format(','.join(analysis['iocs']))
+        string += f"<{self.fame_base_url}/analyses/{analysis['_id']}|See analysis>\n"
 
-        if len(analysis['probable_names']) > 0:
-            string += "Probable Names: {0}\n".format(','.join(analysis['probable_names']))
+        if analysis["iocs"]:
+            string += "\n| Observable | Tags |\n"
+            string += "|:-----------|:-----|\n"
 
-        string += "<{0}/analyses/{1}|See analysis>\n\n".format(self.fame_base_url, analysis['_id'])
-        array =  "| modules used | execution status | TBD                                   |\n"
-        array += "|:-----------|:-----------:|:-----------------------------------------------|\n"
-        for module in analysis['modules']:
-            executed = ':ok_hand: executed' if module in analysis['executed_modules'] else ':rage2: execution failed'
-            array += "| {0}     | {1}         | TBD                           |\n".format(module,executed)
-        data = {'text': string + array }
-        requests.post(self.url, data={'payload': json.dumps(data)})
+            for ioc in analysis["iocs"]:
+                string += f"|{defang(ioc['value'])}|{', '.join(ioc['tags'])}|\n"
+
+        string += "\n| Module | Status |\n"
+        string += "|:-------|:------:|\n"
+
+        for module in analysis["executed_modules"]:
+            string += f"|{module}| :ok_hand: executed |\n"
+
+        for module in analysis["canceled_modules"]:
+            string += f"|{module}| :rage2: canceled |\n"
+
+        data = {"text": string}
+        requests.post(self.url, data={"payload": json.dumps(data)})
