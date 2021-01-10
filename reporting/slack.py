debedd46e1e050ac2fc93c568c6edf69a852eb74
Gael Muller <muller.gael@gmail.com>
2021-01-10 14:23:18 +0100

    feat: port reporting modules

--- reporting/slack.py
+++ reporting/slack.py
@@ -1,4 +1,3 @@
-
 import json
 
 from fame.common.exceptions import ModuleInitializationError
@@ -6,12 +5,14 @@ from fame.core.module import ReportingModule
 
 try:
     import requests
+
     HAVE_REQUESTS = True
 except ImportError:
     HAVE_REQUESTS = False
 
 try:
     from defang import defang
+
     HAVE_DEFANG = True
 except ImportError:
     HAVE_DEFANG = False
@@ -22,15 +23,11 @@ class Slack(ReportingModule):
     description = "Post message on Slack when an anlysis if finished."
 
     config = [
+        {"name": "url", "type": "str", "description": "Incoming webhook URL."},
         {
-            'name': 'url',
-            'type': 'str',
-            'description': 'Incoming webhook URL.'
-        },
-        {
-            'name': 'fame_base_url',
-            'type': 'str',
-            'description': 'Base URL of your FAME instance, as you want it to appear in links.'
+            "name": "fame_base_url",
+            "type": "str",
+            "description": "Base URL of your FAME instance, as you want it to appear in links.",
         },
     ]
 
@@ -48,18 +45,20 @@ class Slack(ReportingModule):
 
     def done(self, analysis):
 
-        string = "Just finished analysis on {0}\n".format(defang(', '.join(analysis._file['names'])))
+        string = "Just finished analysis on {0}\n".format(
+            defang(", ".join(analysis._file["names"]))
+        )
 
-        if analysis['modules'] is not None:
-            string += "Target: {0}\n".format(analysis['modules'])
+        if analysis["modules"]:
+            string += f"Target: {', '.join(analysis['modules'])}\n"
 
-        if len(analysis['extractions']) > 0:
-            string += "Extractions: {0}\n".format(','.join([x['label'] for x in analysis['extractions']]))
+        if analysis["extractions"]:
+            string += f"Extractions: {', '.join([x['label'] for x in analysis['extractions']])}\n"
 
-        if len(analysis['probable_names']) > 0:
-            string += "Probable Names: {0}\n".format(','.join(analysis['probable_names']))
+        if analysis["probable_names"]:
+            string += f"Probable Names: {', '.join(analysis['probable_names'])}\n"
 
-        string += "<{0}/analyses/{1}|See analysis>".format(self.fame_base_url, analysis['_id'])
+        string += f"<{self.fame_base_url}/analyses/{analysis['_id']}|See analysis>"
 
-        data = {'text': string}
-        requests.post(self.url, data={'payload': json.dumps(data)})
+        data = {"text": string}
+        requests.post(self.url, data={"payload": json.dumps(data)})
