d7610571f293ab43390e883ef6d0c769f73a6863
Augustin-FL <Augustin-FL@users.noreply.github.com>
2023-09-13 13:54:57 +0200

    url_preview: use new headless mode

--- processing/url_preview/docker/script.js
+++ processing/url_preview/docker/script.js
@@ -4,7 +4,7 @@ var userurl = process.argv[2];
 var userwait = Number(process.argv[3]);
 
 (async () => {
-    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox'], headless: 'new'});
     const page = await browser.newPage();
     await page.setViewport({'width': 1920, 'height': 1080});
     await page.setRequestInterception(true)
