e1b47ae439c27cb09a0a3c36d7090ff8b0d3cf98
Augustin-FL <Augustin-FL@users.noreply.github.com>
2024-01-10 10:54:48 +0100

    Ignore zbar "Assertion failed" messages

--- processing/qrcode_extractor/docker/script.py
+++ processing/qrcode_extractor/docker/script.py
@@ -1,26 +1,38 @@
 #!/usr/bin/env python3
 import sys
+import os
 import cv2
 from pyzbar.pyzbar import decode
+
+def redirect_stderr(fd, to):
+    sys.stderr.close()
+    os.dup2(to.fileno(), fd)
+    sys.stderr = os.fdopen(fd, 'w')
 
 
 def main(img):
     qrcode_data = set()
+    fd = sys.stderr.fileno()
+    old_stderr = os.fdopen(os.dup(fd), 'w')
 
+    redirect_stderr(fd, open(os.devnull, 'w'))
     # attempt QR code decoding via opencv
     image = cv2.imread(img)
     detect = cv2.QRCodeDetector()
     try:
         retval, decoded, points, straight_qr = detect.detectAndDecodeMulti(image)
+        redirect_stderr(fd, old_stderr)
         if retval:
             qrcode_data |= set(decoded)
     except cv2.error as e:
         pass
 
     # attempt QR code decoding via zbar
+    redirect_stderr(fd, open(os.devnull, 'w'))
     image = cv2.imread(img, 0)
     try:
         value = decode(image)
+        redirect_stderr(fd, old_stderr)
         qrcode_data |= set([v.data.decode() for v in value])
     except TypeError:
         pass
